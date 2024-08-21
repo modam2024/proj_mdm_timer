3dd20bcee4ddc80deb03784a96ddf7881932b477
polomono <polomono@naver.com>
2024-08-21 14:56:36 +0900

    Remove all files from index

--- serve.py
+++
@@ -1,9 +0,0 @@
-from waitress import serve
-from proj_mdm_timer.wsgi import application
-
-print("Django Server Started")
-
-try:
-    serve(application, host='0.0.0.0', port=8000)
-except Exception as e:
-    print("Server Error : ", e)
