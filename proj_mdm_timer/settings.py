472e5a6bd263560ec296da6df71c4d795131ebf0
modamtech <modam2024.cuk.edu>
2024-08-21 19:49:14 +0900

    first

--- proj_mdm_timer/settings.py
+++ proj_mdm_timer/settings.py
@@ -17,6 +17,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-!!#!b!l+%0@@%1uj#&9snl)i6^*!!z$tci^044%a&em)iq*0)k'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
